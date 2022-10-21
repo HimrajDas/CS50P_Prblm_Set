887ac3c67e48ec2e4bc793e5c114aadfa31a4e75
HIMRAJ <himrajdas7@gmail.com>
2022-10-21 15:09:12 +0530

    taqueria.py completed

---
+++ prblm_set3/taqueria.py
@@ -0,0 +1,29 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00,
+}
+
+total_price = 0
+
+while True:
+    try:
+        order = input("Item: ").title()
+        total_price += menu[order]
+        print(f"${total_price}")
+    except KeyError:
+        pass
+    except EOFError:
+            quit()
+
+
+
+           
+
+
