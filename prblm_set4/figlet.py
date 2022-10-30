1533de40b19f7c3d6d954aad0e9335a708a4fa2c
HIMRAJ <himrajdas7@gmail.com>
2022-10-31 01:57:10 +0530

    figlet.py optimized

--- prblm_set4/figlet.py
+++ prblm_set4/figlet.py
@@ -4,7 +4,7 @@ import sys
 user_input = input("Input: ")
 if len(sys.argv) == 1:
     txt_to_ascii = pyfiglet.figlet_format(user_input)
-    print(f"Output: \n{txt_to_ascii}")
 else:
     txt_to_ascii = pyfiglet.figlet_format(user_input, font=sys.argv[-1])
-    print(f"Output: \n{txt_to_ascii}")
+    
+print(f"Output: \n{txt_to_ascii}")
