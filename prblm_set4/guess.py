30c91c7f297cedfc04a292a4199b6af3fea3753e
HIMRAJ <himrajdas7@gmail.com>
2022-11-10 02:47:12 +0530

    guess.py added --bug->str() input type gives error

---
+++ prblm_set4/guess.py
@@ -0,0 +1,23 @@
+import random
+import pyfiglet
+
+
+game_name = pyfiglet.figlet_format("guess The Number")
+print(game_name)
+
+while True:
+    try:
+        level = int(input("Level: "))
+        guess_number = int(input("Guess: "))
+    except level < 0:
+        continue
+    else:
+        rndm_number = random.randint(1, level)
+        print(rndm_number)
+        if guess_number > rndm_number:
+            print("Too large!")
+        elif guess_number < rndm_number:
+            print("Too small!")
+        else:
+            print("Just right!")
+        break
